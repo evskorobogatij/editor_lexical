cb800e239f2c55f2912edc59538e73a4b8d55e55
Евгений С <evskorobogatij@gmail.com>
2023-02-01 01:17:57 +0300

    Заглушка для всплывающей панели инструентов

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,7 +1,5 @@
 // import ExampleTheme from "./themes/ExampleTheme";
-import {
-  LexicalComposer,
-} from "@lexical/react/LexicalComposer";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
@@ -12,23 +10,37 @@ import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin";
 // import AutoLinkPlugin from "@lexical/react/LexicalAutoLinkPlugin";
 import { ListPlugin } from "@lexical/react/LexicalListPlugin";
-
+import FloatingTextFormatToolbarPlugin from "./plugins";
 
 import "./style.css";
 import { editorConfig } from "./config";
+import { useState } from "react";
 
 function Placeholder() {
   return <div className="editor-placeholder">Введите текст...</div>;
 }
 
 export function Editor() {
+  const [floatingAnchorElem, setFloatingAnchorElem] =
+    useState<HTMLDivElement | null>(null);
+
+  const onRef = (_floatingAnchorElem: HTMLDivElement) => {
+    if (_floatingAnchorElem !== null) {
+      setFloatingAnchorElem(_floatingAnchorElem);
+    }
+  };
+
   return (
     <LexicalComposer initialConfig={editorConfig}>
       <div className="editor-container">
         {/* <ToolbarPlugin /> */}
         <div className="editor-inner">
           <RichTextPlugin
-            contentEditable={<ContentEditable className="editor-input" />}
+            contentEditable={
+              <div ref={onRef}>
+                <ContentEditable className="editor-input" />
+              </div>
+            }
             placeholder={<Placeholder />}
             ErrorBoundary={LexicalErrorBoundary}
           />
@@ -38,9 +50,15 @@ export function Editor() {
           {/* <CodeHighlightPlugin /> */}
           <ListPlugin />
           <LinkPlugin />
+          {floatingAnchorElem && (
+            <>
+              <FloatingTextFormatToolbarPlugin
+                anchorElem={floatingAnchorElem}
+              />
+            </>
+          )}
           {/* <AutoLinkPlugin />  */}
           {/* <ListMaxIndentLevelPlugin maxDepth={7} /> */}
-          
         </div>
       </div>
     </LexicalComposer>
