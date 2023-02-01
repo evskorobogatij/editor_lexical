73de34847da412311b58730348d9cd01e823f2b4
Скоробогатый Евгений <evskorobogatij@gmail.com>
2023-02-01 16:57:44 +0300

    Значки

---
+++ src/components/Editor/icons/CheckList.tsx
@@ -0,0 +1,49 @@
+export const CheckListIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9 8H18"
+      stroke="white"
+      stroke-width="1.3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M9 12H18"
+      stroke="white"
+      stroke-width="1.3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M9 16H18"
+      stroke="white"
+      stroke-width="1.3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M5 7.75L5.59193 8.41592C5.6317 8.46067 5.70163 8.46067 5.74141 8.41592L7 7"
+      stroke="white"
+      stroke-width="0.7"
+      stroke-linecap="round"
+    />
+    <path
+      d="M5 11.75L5.59193 12.4159C5.6317 12.4607 5.70163 12.4607 5.74141 12.4159L7 11"
+      stroke="white"
+      stroke-width="0.7"
+      stroke-linecap="round"
+    />
+    <path
+      d="M5 15.75L5.59193 16.4159C5.6317 16.4607 5.70163 16.4607 5.74141 16.4159L7 15"
+      stroke="white"
+      stroke-width="0.7"
+      stroke-linecap="round"
+    />
+  </svg>
+);
---
+++ src/components/Editor/icons/MarkedList.tsx
@@ -0,0 +1,52 @@
+export const MarkedListIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9 8H18"
+      stroke="white"
+      stroke-width="1.3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M9 12H18"
+      stroke="white"
+      stroke-width="1.3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M9 16H18"
+      stroke="white"
+      stroke-width="1.3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M6 8H6.0075"
+      stroke="white"
+      stroke-width="1.3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M6 12H6.0075"
+      stroke="white"
+      stroke-width="1.3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M6 16H6.0075"
+      stroke="white"
+      stroke-width="1.3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
