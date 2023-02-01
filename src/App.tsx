3595808f838e30aedce3675bef43be4710cd6c42
Скоробогатый Евгений <evskorobogatij@gmail.com>
2023-02-01 14:50:37 +0300

    Изменения в оформлении контейнера

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { Editor } from "./components/Editor";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     // <div className="App">
